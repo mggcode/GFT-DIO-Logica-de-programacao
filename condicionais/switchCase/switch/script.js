4973773de22bd0741d57e0c426e7cf03385fa9c6
M. G. <60014891+MGBrave@users.noreply.github.com>
2024-11-11 14:06:20 -0300

    elseif

---
+++ condicionais/switchCase/switch/script.js
@@ -0,0 +1,24 @@
+//estrutura de decisão
+//switch/case /break/default
+
+
+let fruta = "banana"
+
+
+switch(fruta){
+    case "laranja":
+        console.log("suco laranja")
+        break
+
+        case "banana":
+        console.log("vitamina banana")
+        break
+
+        case "maça" :
+            console.log("suco maça")
+            break
+
+        
+        default:
+            console.log("suco generico")
+}
